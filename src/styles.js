bc9a3313a400b6d5c3cf7395d6c0ccfdfe694be8
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-01-30 23:51:01 +0000

    create new expo app

---
+++ src/styles.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line import/namespace
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
