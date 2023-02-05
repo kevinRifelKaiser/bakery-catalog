dda364abe42f3a54f3b677747a8dba3274a111af
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-02-05 18:05:20 +0000

    [feature] - stylize the header

--- src/navigation/shop.jsx
+++ src/navigation/shop.jsx
@@ -1,12 +1,24 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { theme } from '../constants/theme/index';
 import { Categories, ProductDetail, Products } from '../screens/index';
 
 const Stack = createNativeStackNavigator();
 
 const ShopNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Categories">
+    <Stack.Navigator
+      initialRouteName="Categories"
+      screenOptions={{
+        headerTitleStyle: {
+          fontFamily: 'Rubik-Medium',
+          fontSize: 20,
+        },
+        headerTintColor: theme.colors.white,
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+      }}>
       <Stack.Screen name="Categories" component={Categories} options={{ title: 'Categories' }} />
       <Stack.Screen
         name="ProductDetail"
